7431c679715128956566270da8cd9edf834a7c1a
angekasa <aggelikik019@gmail.com>
2025-07-22 09:54:26 +0300

    Add files via upload

---
+++ reputable_submitters.py
@@ -0,0 +1,9 @@
+reputable_submitters = {
+    "GeneDx",
+    "Invitae",
+    "Ambry Genetics",
+    "Color Health",
+    "Blueprint Genetics",
+    "Laboratory for Molecular Medicine",
+    "University of Chicago Genetic Services"
+}
