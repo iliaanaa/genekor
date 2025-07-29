7274464be42e00aea7a26aaf9a99a7975f2a2cc1
ChrisBibo <cpatainxim@gmail.com>
2025-07-29 10:29:34 +0300

    Διόρθωση στο dataparse

--- missense_count_sql_questions.sql
+++ missense_count_sql_questions.sql
@@ -1,27 +1,27 @@
 --Πόσες Missense παραλλαγές έχει στην clinvar για το BRCA1
 SELECT COUNT(*) AS missense_count
-FROM clinvar_data
-WHERE gene = 'BRCA1'
-    AND consequence = 'missense_variant';
+FROM gene_variants
+WHERE gene_symbol = 'KLHL10'
+    AND molecular_consequence = 'missense';
 
 --Πόσες Missense παραλλαγές έχει στην clinvar για το BRCA1 που είναι παθογόνες
 SELECT COUNT(*) AS pathogenic_missense_count
-FROM clinvar_data
-WHERE gene = 'BRCA1'
-    AND consequence = 'missense_variant'
+FROM gene_variants
+WHERE gene_symbol = 'KLHL10'
+    AND molecular_consequence = 'missense'
     AND clinical_significance = 'Pathogenic';
 
 --Πόσες Missense παραλλαγές έχει στην clinvar για το BRCA1 που είναι παθογόνες και βρίσκονται μεταξύ των αμινοξέων 1756-1855
 SELECT COUNT(*) AS pathogenic_missense_count_in_range
-FROM clinvar_data
-WHERE gene = 'BRCA1'
-    AND consequence = 'missense_variant'
-    AND clinical_significance = 'Pathogenic'
-    AND protein_position BETWEEN 1756 AND 1855;
+FROM gene_variants
+WHERE gene_symbol  = 'KLHL10'
+AND molecular_consequence = 'missense'
+AND clinical_significance = 'Pathogenic'
+AND protein_pos BETWEEN 1756 AND 1855;
 
 --Πόσες Missense παραλλαγές έχει στην clinvar για το BRCA1 στην θέση 34 της πρωτεΐνης
 SELECT COUNT(*) AS missense_count_at_position
-FROM clinvar_data
-WHERE gene = 'BRCA1'
-    AND consequence = 'missense_variant'
-    AND protein_position = 34;
+FROM gene_variants
+WHERE gene_symbol = 'KLHL10'
+    AND molecular_consequence = 'missense'
+    AND protein_pos = 34;
