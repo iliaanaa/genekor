2763cb5df762199024e6cb4614f0a0646f923380
ChrisBibo <cpatainxim@gmail.com>
2025-08-01 11:01:52 +0300

    SQL_erwthmata updated gia TP53 mhn kourazeste kiolas

--- missense_count_sql_questions.sql
+++ missense_count_sql_questions.sql
@@ -1,20 +1,20 @@
 --Πόσες Missense παραλλαγές έχει στην clinvar για το BRCA1
 SELECT COUNT(*) AS missense_count
 FROM gene_variants
-WHERE gene_symbol = 'KLHL10'
+WHERE gene_symbol = 'TP53'
     AND molecular_consequence = 'missense';
 
 --Πόσες Missense παραλλαγές έχει στην clinvar για το BRCA1 που είναι παθογόνες
 SELECT COUNT(*) AS pathogenic_missense_count
 FROM gene_variants
-WHERE gene_symbol = 'KLHL10'
+WHERE gene_symbol = 'TP53'
     AND molecular_consequence = 'missense'
     AND clinical_significance = 'Pathogenic';
 
 --Πόσες Missense παραλλαγές έχει στην clinvar για το BRCA1 που είναι παθογόνες και βρίσκονται μεταξύ των αμινοξέων 1756-1855
 SELECT COUNT(*) AS pathogenic_missense_count_in_range
 FROM gene_variants
-WHERE gene_symbol  = 'KLHL10'
+WHERE gene_symbol  = 'TP53'
 AND molecular_consequence = 'missense'
 AND clinical_significance = 'Pathogenic'
 AND protein_pos BETWEEN 1756 AND 1855;
@@ -22,6 +22,6 @@ AND protein_pos BETWEEN 1756 AND 1855;
 --Πόσες Missense παραλλαγές έχει στην clinvar για το BRCA1 στην θέση 34 της πρωτεΐνης
 SELECT COUNT(*) AS missense_count_at_position
 FROM gene_variants
-WHERE gene_symbol = 'KLHL10'
+WHERE gene_symbol = 'TP53'
     AND molecular_consequence = 'missense'
     AND protein_pos = 34;
