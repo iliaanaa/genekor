9b70debe6005ee51154f027a1acaf7266ecbad50
angekasa <aggelikik019@gmail.com>
2025-08-04 09:49:59 +0300

    Create test_subjects.py

---
+++ test_subjects.py
@@ -0,0 +1,76 @@
+test_subjects ={
+    'c.1182A>G p.Ter394Trp', # Uncertain significance, PM5
+    'c.1111_1181dup p.Ter394CysextTer?', # Uncertain significance, PM5
+    'c.1182A>T p.Ter394Cys', # Uncertain sigificance, PM5
+    'c.1181G>T p.Ter394Leu', # Uncertain significance, PM5
+    'c.1180T>A p.Ter394Arg', # Uncertain significance, PM5
+    'c.1180T>G p.Ter394Gly', # Uncertain significance, PM5
+    'c.1179dup p.Ter394LeuextTer?', # Conflicting classifications, PM5
+
+    'c.1051A>G p.Lys351Glu', # Uncertain significance, PM5
+    'c.1051A>T p.Lys351Ter', # Pathogenic, PM5
+
+    'c.1040C>T p.Ala347Asp', # Uncertain significance, PS1
+    'c.1040C>A p.Ala347Asp', # Pathogenic, PS1
+
+    'c.1036G>A p.Glu346Lys', # conflicting classifications, PM5
+    'c.1036G>T p.Glu346Ter', # pathogenic, PM5
+    'c.1036del p.Glu346fs', # pathogenic, PM5
+
+    'c.1025G>T p.Arg342Leu', # uncertain significance, PM5
+    'c.1025G>A p.Arg342Gln', # likely benign, PM5
+    'c.1025G>C p.Arg342Pro', # pathogenic, PM5
+    'c.1024C>G p.Arg324Gly', # uncertain significance, PM5
+    'c.1024del p.Arg324fs', # pathogenic, PM5
+    'c.1024C>T p.Arg324Ter', # pathogenic, PM5
+
+    'c.1023C>A p.Phe341Leu', # conflicting classifications, PS1, PM5
+    'c.1023C>G p.Phe341Leu', # likely benign, PS1, PM5
+    'c.1023C>T p.Phe341=', # benign/likely benign, PM5
+    'c.1022del p.Phe341fs', # pathogenic, PS1, PM5
+    'c.1021_1022delinsAG p.Phe341Ser', # likely pathogenic, PM5
+    'c.1021T>A p.Phe341Ile', # Uncertain significance, PM5
+    'c.1021delinsGAA p.Phe341fs', # pathogenic, PS1, PM5
+
+    'c.1011C>G p.Arg337=', # likely benign, PS1, PM5
+    'c.1011C>T p.Arg337=', # benign/likely benign, PS1, PM5
+    'c.1010_1011del p.Arg337fs', #pathogenic, PS1, PM5
+    'c.1010G>C p.Arg337Pro', # likely pathogenic, PM5
+    'c.1010G>A p.Arg337His', # pathogenic, PM5
+    'c.1007_1008dup p.Arg337fs', # pathogenic, PS1, PM5
+    'c.1009C>T p.Arg337Cys', # pathogenic, PS1, PM5
+    'c.1010G>T p.Arg337Leu', # likely pathogenic, PM5
+    'c.1009C>A p.Arg337Ser', # uncertain significance, PM5
+    'c.1009C>G p.Arg337Cys', # likely pathogenic, PS1, PM5
+    'c.1009C>T p.Arg337Cys', # pathogenic & oncogenic, PS1, PM5
+
+    'c.990_993del p.Glu331fs', # pathogenic, PS1, PM5
+    'c.993G>A p.Glu331=', # pathogenic/likely pathogenic & likely oncogenic, PM5
+    'c.992A>G p.Glu331Arg', # conflicting classifications, PM5
+    'c.991C>G p.Glu331Gln', # conflicting classifications, PM5
+    'c.991C>A p.Glu331Lys', # uncertain significance, PM5
+    'c.991C>T p.Glu331Ter', # pathogenic & oncogenic, PM5
+    'c.990del p.Glu331fs', # pathogenic, PS1, PM5
+
+    'c.951G>A p.Glu317=', # benign/likely benign, PM5
+    'c.951G>T p.Glu317His', # Conflicting classifications, PM5
+    'c.950A>T p.Glu317Leu', # Uncertain significance, PM5
+    'c.950A>G p.Glu317Arg', # Conflicting classifications, PM5
+    'c.950del p.Glu317fs', # pathogenic, PS1, PM5
+    'c.949del p.Glu317fs', # pathogenic, PS1, PM5
+    'c.949C>T p.Glu317Ter', # pathogenic/likely pathogenic &  oncogenic, PM5
+    'c.949C>A p.Glu317Lys', # benign, PM5
+
+    'c.839_842dup p.Asp281fs', # pathogenic, PS1, PM5
+    'c.843C>T p.Asp281=', # benign/likely benign, PM5
+    'c.842_843insG p.Asp281fs', # pathogenic, PS1, PM5
+    'c.843C>A p.Asp281Glu', # pathogenic/likely pathogenic & oncogenic, PS1, PM5
+    'c.843C>G p.Asp281Glu', # conflicting classifications & oncogenic, PS1, PM5
+    'c.841_842del p.Asp281fs', # pathogenic, PS1, PM5
+    'c.842A>C p.Asp281Ala', # pathogenic/likely pathogenic, PM5
+    'c.842A>T p.Asp281Val', # pathogenic/likely pathogenic, PM5
+    'c.842A>G p.Asp281Gly', # pathogenic, PM5
+    'c.841G>C p.Asp281His', # pathogeniv/likely pathogenic, PM5
+    'c.841G>A p.Asp281Asn', # pathogenic/likely pathogenic & oncogenic, PM5
+    'c.841G>T p.Asp281Tyr', # pathogenic/likely pathogenic & oncogenic, PM5
+}
