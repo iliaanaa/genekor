646f5b803b9506b725338643233880bcc0a94658
angekasa <aggelikik019@gmail.com>
2025-10-11 11:41:09 +0300

    deleted new.py main.py load_from_csv.py krithria2.py krithria.py knonw_pathogenic_dict.py from main

--- load_from_csv.py
+++
@@ -1,4 +0,0 @@
-df_loaded = pd.read_csv('brca_acmg_variants.csv')
-# Μετατροπή λιστών από strings (αν χρειάζεται)
-import ast
-df_loaded['acmg_criteria'] = df_loaded['acmg_criteria'].apply(ast.literal_eval)
